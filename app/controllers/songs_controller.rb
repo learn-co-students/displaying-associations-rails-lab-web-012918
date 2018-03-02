bf288566d86906dec553d6b805623faa48847b00
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-03-02 15:53:54 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,16 @@
 class SongsController < ApplicationController
+
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
+    @artist = @song.artist_name
   end
 
   def new
+    @song = Song.new
   end
 
   def create
@@ -47,4 +52,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title)
   end
 end
-
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def display
+    "#{self.artist_name} - #{self.title}"
+  end
 end
